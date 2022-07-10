4c6820ee8bdf9c505b64c40d316b640cae90a043
KAKUSI <40749@163.com>
2022-07-10 22:20:06 +0800

    mod1 finish

---
+++ mod1/js/script.js
@@ -0,0 +1,18 @@
+var user_name = new Vue({
+    el: "#user_name",
+    data: {
+        value: "AceTaffy21",
+    }
+})
+var traffic = new Vue({
+    el: "#traffic",
+    data: {
+        value: "8.12G",
+    }
+})
+var connect_time = new Vue({
+    el: "#connect_time",
+    data: {
+        value: "21:06:26",
+    }
+})
